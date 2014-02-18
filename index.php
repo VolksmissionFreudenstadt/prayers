46851e76f56cc389ed866b05be72d6df548ce2f2
Christoph Fischer <chris@toph.de>
2014-02-18 10:08:50 +0100

    First implementation of image creation

--- index.php
+++ index.php
@@ -1,5 +1,83 @@
 <?php
 
+
+
+/**
+* Implement word wrapping... Ughhh... why is this NOT done for me!!!
+* OK... I know the algorithm sucks at efficiency, but it's for short messages, okay?
+*
+* Make sure to set the font on the ImagickDraw Object first!
+* @author BMiner
+* @link http://stackoverflow.com/questions/5746537/how-can-i-draw-wrapped-text-using-imagick-in-php/5746551#5746551
+* @param image the Imagick Image Object
+* @param draw the ImagickDraw Object
+* @param text the text you want to wrap
+* @param maxWidth the maximum width in pixels for your wrapped "virtual" text box
+* @return an array of lines and line heights
+*/
+function wordWrapAnnotation(&$image, &$draw, $text, $maxWidth)
+{
+    $words = explode(" ", $text);
+    $lines = array();
+    $i = 0;
+    $lineHeight = 0;
+    while($i < count($words) )
+    {
+        $currentLine = $words[$i];
+        if($i+1 >= count($words))
+        {
+            $lines[] = $currentLine;
+            break;
+        }
+        //Check to see if we can add another word to this line
+        $metrics = $image->queryFontMetrics($draw, $currentLine . ' ' . $words[$i+1]);
+        while($metrics['textWidth'] <= $maxWidth)
+        {
+            //If so, do it and keep doing it!
+            $currentLine .= ' ' . $words[++$i];
+            if($i+1 >= count($words))
+                break;
+            $metrics = $image->queryFontMetrics($draw, $currentLine . ' ' . $words[$i+1]);
+        }
+        //We can't add the next word to this line, so loop to the next line
+        $lines[] = $currentLine;
+        $i++;
+        //Finally, update line height
+        if($metrics['textHeight'] > $lineHeight)
+            $lineHeight = $metrics['textHeight'];
+    }
+    return array($lines, $lineHeight);
+}
+
+
+function createImage ($config, $text) {
+	// create the image
+	$img = new Imagick();
+	$img->newImage (1024, 768, new ImagickPixel('white'));
+	$img->setImageFormat('jpeg');
+	
+	// font settings
+	$draw = new ImagickDraw();
+	$draw->setFillColor('black');
+	$draw->setFont(dirname(__FILE__).'/fonts/OpenSans-Regular.ttf');
+	$draw->setFontSize(43);
+	
+	// first text
+	$img->annotateImage($draw, 30, 40, 0, $test);
+	
+
+	// write the image to the output folder
+	$fileBaseName = $config['output']['prefix'].str_pad($index, 3, '0', STR_PAD_LEFT).'.jpg';
+	$fileName = $config['output']['path'].'/'.$fileBaseName;
+	//echo 'Erstelle Folie als '.$fileName.' ...<br />';
+	$img->writeImage($fileName);
+	
+	return $fileBaseName;
+}
+
+
+//================================================================================================
+
 $config = yaml_parse_file('config.yaml');
 
 $db = new mysqli ($config['kOOL']['db']['host'], 
@@ -33,4 +111,8 @@ foreach ($pp as $key => $val) {
 
 $text = join ('; ', $pp);
 
-die ($text);
+//die ($text);
+$fn = createImage($config, $text);
+echo '<img src="'.$fn.'" />';
+
+
