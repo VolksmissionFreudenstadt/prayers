65759a84f80a1bd4dfa42d535c90ad70212c6b37
Christoph Fischer <chris@toph.de>
2014-02-18 11:19:14 +0100

    FIXED: typo

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ $text = join ('; ', $pp);
 
 // get people for blessing
 
-$res = $db->query('SELECT title FROM `ko_event` WHERE `eventgruppen_id`='.$config['kOOL']['blessing']['group'].' and `startdatum`=\''.strtime('%Y-%m-%d', $startDate).'\'');
+$res = $db->query('SELECT title FROM `ko_event` WHERE `eventgruppen_id`='.$config['kOOL']['blessing']['group'].' and `startdatum`=\''.strftime('%Y-%m-%d', $startDate).'\'');
 list($blessing) = $res->fetch_assoc();
 
 
